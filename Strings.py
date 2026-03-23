3b3de2ecb9cf1a6530c1fdd7e59669a636ca7481
VeeramalluDinesh <dineshabhiram500@gmail.com>
2026-03-23 17:45:55 +0530

    Strings.py

---
+++ Strings.py
@@ -0,0 +1,27 @@
+x='Dinesh'
+y="Abhiram"
+print(x,y)
+
+#triple cotes for multiple strings
+a="""Nowdays the society is following AI trend""" 
+print(a)
+print(x[2]) #At specific position
+print(len(y)) #for length of string
+#string slicing
+print(x[:5])
+print(y[3:6])
+print(y[2:-5])
+
+#Modifying the strings
+z="Happy days"
+print(z.upper())
+print(z.lower())
+print(z.strip())
+print(z.replace("H","h"))
+print(z.split(""))
+
+#string concatenation 
+print(x+y)
+print(x+" "+y)
+c=20
+print(f"{x}:{c})

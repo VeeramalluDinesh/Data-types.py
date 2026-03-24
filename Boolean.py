a4f1896837e2cf5cdd0d582745d203f8ac3d07a8
VeeramalluDinesh <dineshabhiram500@gmail.com>
2026-03-24 22:33:19 +0530

    Boolean.py

---
+++ Boolean.py
@@ -0,0 +1,20 @@
+#Boolean datatype
+x=10
+y=30
+print(x>y)
+print(x<=y)
+print(bool("Hello"))
+print(bool("15"))
+print(bool(20))
+print(bool(0))
+print(bool(None))
+print(bool("")) # empty string
+print(bool({})) # empty dictionary 
+print(bool([])) # empty list
+#  In Function
+def myfunc():
+ return True
+print(myfunc())
+#using isinstance
+x="Dinesh"
+print(isinstance(x,str)) # It decides declare variable is a correct datatype or not 
